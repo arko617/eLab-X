4822368fb9d18af1207fc0504115e199e56a368c
Johnny Ho <johnny5550822@gmail.com>
2015-11-04 02:46:08 -0800

    Connect to dropbox client and google drive client

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 angular.module('eLabXApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'HomeCtrl', 'HomeService', 'AccountCtrl', 'AccountService','ManageCtrl','ManageService']);
+
+// Get apis oauth here
+
+//--------------Google APIs information
+var CLIENT_ID = '102710631798-lb77ojeikkaa0dsjcuvq08igkd8llrjh.apps.googleusercontent.com';	//for johnny5550822
+//var CLINET_ID = '900888398054.apps.googleusercontent.com';	//For labbook2013 account
+var SCOPES = ['https://www.googleapis.com/auth/drive.install','https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/plus.login'];	//Different scope have different meaning, see https://developers.google.com/drive/scopes
+var apiKey = "AIzaSyB71KszVh0JIH-7EXVgmTv8vt2uy33lPMU";
+
+
+
+//--------------Dropbox APIs information
+//-----Documentation: https://github.com/dropbox/dropbox-js
+//-----To be more specific: https://github.com/dropbox/dropbox-js/blob/stable/guides/getting_started.md
+var dropboxAPIKey = 'r2k206ydj5s5kv1';
+var dropBoxClient = new Dropbox.Client({ key: dropboxAPIKey });
+console.log("Connecting to Dropbox Client...",dropBoxClient);
